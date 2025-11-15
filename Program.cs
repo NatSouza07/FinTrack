0f67d0a5909dfb4f128899a2b52abfa29e19cd19
Natã do Carmo Souza <nata.carmo05@gmail.com>
2025-11-15 10:31:44 -0300

    chore: configure FinTrackConnection connection string
    Adicionada connection string 'FinTrackConnection' ao appsettings.json utilizando SQL Server (.\\SQLExpress).
    Contexto configurado no Program.cs para uso com Entity Framework Core.

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<FinTrackContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("FinTrackConnection")));
 
 var app = builder.Build();
 
